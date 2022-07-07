19004eed6db0d611d74be23bc9741f0acc0825e9
Andrew Guo <andrew.c.guo@gmail.com>
2022-07-07 16:56:44 -0400

    Done lab 2

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn validate_grading_system() {
 
 /// Calculate the grade
 fn calculate_grade(carrots: i32, nuts: i32, seeds: i32) -> i32 {
-    todo!()
+    return (carrots + nuts) * (carrots + nuts + seeds);
 }
 
 /// Validate the safety system
@@ -49,7 +49,7 @@ fn calculate_safety_status(
     has_carrot: bool,
     friends_nearby: i32,
 ) -> bool {
-    todo!()
+    return !wolves_nearby && day_time || has_carrot || friends_nearby > 3;
 }
 
 /// Validate the simulation
@@ -66,8 +66,19 @@ fn validate_simulation() {
 }
 
 /// Simulate the rabbit population
-fn simulate(starting_rabbits: i128) -> i32 {
-    todo!()
+fn simulate(mut starting_rabbits: i128) -> i32 {
+    let mut days: i32 = 0;
+
+    while starting_rabbits != 1 {
+        starting_rabbits = if starting_rabbits % 2 == 1 {
+            (3 * starting_rabbits) + 1
+        } else {
+            starting_rabbits / 2
+        };
+        days += 1;
+    }
+
+    return days;
 }
 
 #[cfg(test)]
